64cbf11cae239f807018f6d82cdc392a5a442536
tthau <tthau@cmc.com.vn>
2025-06-17 07:01:56 +0000

    Remove ECR and ECS modules

--- modules/repositories/main.tf
+++
@@ -1,9 +0,0 @@
-# modules/repositories/main.tf
-resource "aws_codecommit_repository" "repos" {
-  for_each = var.repositories
-
-  repository_name = "${var.project}-${var.environment}-${each.value.name}"
-  description     = each.value.description
-  default_branch  = each.value.branch
-  tags            = local.tags
-}
--- modules/resource-groups/outputs.tf
+++
@@ -1,8 +0,0 @@
-# modules/resource-groups/outputs.tf
-output "group_arns" {
-  value = {
-    app     = aws_resourcegroups_group.app.arn
-    compute = aws_resourcegroups_group.compute.arn
-    network = aws_resourcegroups_group.network.arn
-  }
-}
