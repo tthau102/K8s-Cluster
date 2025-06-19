2d34729e9bce59b320f75103cd840626b748ddad
tth <trunghautran0102@gmail.com>
2025-06-19 09:48:32 +0700

    Update name prefix with Owner tag in ec2 module

--- modules/ec2/locals.tf
+++ modules/ec2/locals.tf
@@ -5,5 +5,5 @@ data "aws_default_tags" "current" {}
 
 locals {
   # Name prefix from provider tags (auto-inherited)
-  name_prefix = "${data.aws_default_tags.current.tags["Project"]}-${data.aws_default_tags.current.tags["Environment"]}"
+  name_prefix = "${data.aws_default_tags.current.tags["Owner"]}-${data.aws_default_tags.current.tags["Project"]}-${data.aws_default_tags.current.tags["Environment"]}"
 }
